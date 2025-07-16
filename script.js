3d0f7b72ed519220867eaa20e69b1f41c7219a41
Binh05 <binhdt0452@gmail.com>
2025-07-16 22:52:35 +0700

    update

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ closeBtn.addEventListener('click', () => {
     menuBtn.style.display = 'block'
 })
 
+// feature
+
 const featureControl = document.querySelector('.features-tabs')
 let featureActive = document.querySelector('.features__tab--active')
+let ftCardActive = document.querySelector('.feature-card--active')
+
+const ftCards = document.querySelectorAll('.feature-card')
+let index = 0
 
 featureControl.addEventListener('click', (e) => {
     const clickedEl = e.target;
@@ -27,4 +33,56 @@ featureControl.addEventListener('click', (e) => {
     clickedTab.classList.add('features__tab--active')
 
     featureActive = clickedTab
+
+    index = Number(featureActive.value) - 1
+
+    ftCardActive.classList.remove('feature-card--active')
+    ftCardActive = ftCards[index]
+    ftCardActive.classList.add('feature-card--active')
+})
+
+// faq
+
+const acdContainer = document.querySelector('.accordion')
+
+acdContainer.addEventListener('click', (e) => {
+    const clicked = e.target
+    const acdClicked = clicked.closest('.accordion__btn')
+    const acdArrow = acdClicked.querySelector('.toggle-icon')
+    
+    if (!acdClicked) return
+
+    const acdText = acdClicked.nextElementSibling
+
+    if (acdText.style.maxHeight) {
+        acdText.style.maxHeight = null
+        acdArrow.classList.remove('toggle-icon--active')
+    }else {
+        acdText.style.maxHeight = `${acdText.scrollHeight}px`
+        acdArrow.classList.add('toggle-icon--active')
+    }
+})
+
+// form valid
+
+const form = document.querySelector('.cta__form')
+const submitForm = document.querySelector('.form__btn')
+const email = document.querySelector('.form__input input')
+
+const checkValidEmail = (email) => {
+    const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    return regex.test(email)
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const valid = checkValidEmail(email.value)
+
+    if (!valid) {
+        form.classList.add('is-valid')
+        return
+    }
+
+    form.classList.remove('is-valid')
 })
